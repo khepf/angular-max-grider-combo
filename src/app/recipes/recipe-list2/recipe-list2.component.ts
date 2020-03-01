f485f743dbf84589cef6cde4194088033d376732
Jeremy Kepf <jeremy1975@gmail.com>
2020-03-01 16:21:37 -0500

    max-64 g-18

--- src/app/recipes/recipe-list2/recipe-list2.component.ts
+++ src/app/recipes/recipe-list2/recipe-list2.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list2',
-  templateUrl: './recipe-list2.component.html',
-  styleUrls: ['./recipe-list2.component.css']
+  selector: "app-recipe-list2",
+  templateUrl: "./recipe-list2.component.html",
+  styleUrls: ["./recipe-list2.component.css"]
 })
 export class RecipeList2Component implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
