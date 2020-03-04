6c571139023660fed0e407bc7382f72ae315f259
Jeremy Kepf <jeremy1975@gmail.com>
2020-03-04 15:13:14 -0500

    break point, start with max 93

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: "app-header",
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/recipes/recipe-list2/recipe-list2.component.ts
+++ src/app/recipes/recipe-list2/recipe-list2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ["./recipe-list2.component.css"]
 })
 export class RecipeList2Component implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       "A Test Recipe",
       "This is simply a test",
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg"
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is also a test recipe",
       "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg"
     )
   ];
@@ -18,4 +24,8 @@ export class RecipeList2Component implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
